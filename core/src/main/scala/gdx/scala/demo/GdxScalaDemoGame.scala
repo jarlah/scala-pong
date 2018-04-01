fae59f3161358fa0ff416b5b9f5952629109f2e7
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-04-01 21:54:53 +0200

    adding paddle and ball

--- core/src/main/scala/gdx/scala/demo/GdxScalaDemoGame.scala
+++ core/src/main/scala/gdx/scala/demo/GdxScalaDemoGame.scala
@@ -2,26 +2,42 @@ package gdx.scala.demo
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.graphics.{Color, GL20, OrthographicCamera, Texture}
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.physics.bullet.Bullet
 
 class GdxScalaDemoGame extends ApplicationAdapter {
-  private[demo] var batch: SpriteBatch = _
-  private[demo] var img: Texture = _
+  var batch: SpriteBatch = _
+  var ballImage: Texture = _
+  var paddleImage: Texture = _
+  var camera: OrthographicCamera = _
+  var paddle: Rectangle = _
+  var ball: Rectangle = _
 
   override def create() {
     Bullet.init()
+    camera = new OrthographicCamera
+    camera.setToOrtho(false, 800, 480)
     batch = new SpriteBatch
-    img = new Texture("badlogic.jpg")
+    ballImage = new Texture("ball.png")
+    paddleImage = new Texture("paddle.png")
+    paddle = new Rectangle(800 / 2 - 80 / 2, 20, 80, 16)
+    ball = new Rectangle(800 / 2 - 16 / 2, 300, 16, 16)
   }
 
   override def render() {
-    Gdx.gl.glClearColor(1, 0, 0, 1)
+    Gdx.gl.glClearColor(135 / 255f, 206 / 255f, 235 / 255f, 1)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
     batch.begin()
-    batch.draw(img, 0, 0)
+    batch.setColor(Color.BLACK)
+    batch.draw(ballImage, ball.x, ball.y)
+    batch.draw(paddleImage, paddle.x, paddle.y)
     batch.end()
+    if(Gdx.input.isKeyPressed(Keys.LEFT))
+      paddle.x -= 200 * Gdx.graphics.getDeltaTime
+    if(Gdx.input.isKeyPressed(Keys.RIGHT))
+      paddle.x += 200 * Gdx.graphics.getDeltaTime
   }
 }
--- desktop/src/main/scala/gdx/scala/demo/desktop/DesktopLauncher.java
+++ desktop/src/main/scala/gdx/scala/demo/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import gdx.scala.demo.GdxScalaDemoGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Pong";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new GdxScalaDemoGame(), config);
 	}
 }
