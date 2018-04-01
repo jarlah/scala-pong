9ae79149a31fea46ce79353d38f7db09991027a7
Jarl André Hübenthal <jarl.andre.hubenthal@moller.no>
2018-04-01 23:38:43 +0200

    formatting

--- desktop/src/main/scala/gdx/scala/demo/desktop/DesktopLauncher.scala
+++ desktop/src/main/scala/gdx/scala/demo/desktop/DesktopLauncher.scala
@@ -5,11 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import gdx.scala.demo.PongGame
 
 object DesktopLauncher extends App {
-    val width = 800
-    val height = 480
-		val config = new LwjglApplicationConfiguration()
-		config.title = "Pong"
-		config.width = width
-		config.height = height
-		new LwjglApplication(new PongGame(width, height), config)
+  val width = 800
+  val height = 480
+  val config = new LwjglApplicationConfiguration()
+  config.title = "Pong"
+  config.width = width
+  config.height = height
+  new LwjglApplication(new PongGame(width, height), config)
 }
