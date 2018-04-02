4dcc5aea08f603694383751b1dc17f0aff42c4a6
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-04-02 14:30:21 +0200

    remove useless type hints

--- core/src/main/scala/gdx/scala/demo/GameScreen.scala
+++ core/src/main/scala/gdx/scala/demo/GameScreen.scala
@@ -5,10 +5,10 @@ import com.badlogic.gdx.graphics.{Color, Texture}
 class GameScreen(game: PongGame) extends AbstractScreen {
 
   val ballImage = new Texture("ball.png")
-  var ball: Ball = Ball.create(game)
+  var ball = Ball.create(game)
 
   val paddleImage = new Texture("paddle.png")
-  var paddle: Paddle = Paddle.create(game)
+  var paddle = Paddle.create(game)
 
   // Mutable state variables
   var ballUp: Boolean = false
@@ -35,32 +35,32 @@ class GameScreen(game: PongGame) extends AbstractScreen {
     }
   }
 
-  def repositionBallAtTop(): Unit = ball = ball.copy(y = game.height - ball.width)
+  def repositionBallAtTop() = ball = ball.copy(y = game.height - ball.width)
 
-  def ballHitsFloor: Boolean = ball.y < 0
+  def ballHitsFloor = ball.y < 0
 
-  def ballHitsCeiling: Boolean = ball.y > game.height - ball.width
+  def ballHitsCeiling = ball.y > game.height - ball.width
 
-  def flipVerticalBallDirection(): Unit = {
+  def flipVerticalBallDirection() = {
     ballUp = !ballUp
     ballDown = !ballDown
   }
 
-  def advancePaddle(delta: Float): Unit = {
+  def advancePaddle(delta: Float) = {
     if (game.isLeftKeyPressed)
       paddle = paddle.copy(x = paddle.x - paddle.speed * delta)
     if (game.isRightKeyPressed)
       paddle = paddle.copy(x = paddle.x + paddle.speed * delta)
   }
 
-  def advanceBall(delta: Float): Unit = {
+  def advanceBall(delta: Float) = {
     if (ballDown)
       ball = ball.copy(y = ball.y - ball.speed * delta)
     if (ballUp)
       ball = ball.copy(y = ball.y + ball.speed * delta)
   }
 
-  override def dispose(): Unit = {
+  override def dispose() = {
     ballImage.dispose()
     paddleImage.dispose()
   }
--- core/src/main/scala/gdx/scala/demo/Paddle.scala
+++ core/src/main/scala/gdx/scala/demo/Paddle.scala
@@ -3,14 +3,14 @@ package gdx.scala.demo
 import com.badlogic.gdx.math.Rectangle
 
 case class Paddle(x: Float, y: Float, width: Int, height: Int, speed: Int = 200) {
-  def rectangle: Rectangle = new Rectangle(x, y, width, height)
-  def overlaps(other: Rectangle): Boolean = rectangle.overlaps(other)
+  def rectangle = new Rectangle(x, y, width, height)
+  def overlaps(other: Rectangle) = rectangle.overlaps(other)
 }
 
 object Paddle {
   val width: Int = 80
   val height: Int = 16
 
-  def create(dimensions: Dimensions, initialPosition: Option[Float] = None): Paddle =
+  def create(dimensions: Dimensions, initialPosition: Option[Float] = None) =
     Paddle(dimensions.width / 2 - width / 2, initialPosition.getOrElse(20f), width, height)
 }
